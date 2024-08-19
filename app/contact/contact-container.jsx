cf883a12de4f2e004ca50c742ec296d2ca7e2160
SandalikaAriyrathna <dilsandu96@gmail.com>
2024-08-20 01:03:52 +0530

    Update contact-container.jsx

--- app/contact/contact-container.jsx
+++ app/contact/contact-container.jsx
@@ -10,7 +10,8 @@ const Contactcontainer = () => {
               <div className="contact__area-title">
                 <h3 className="mb-25">Get In Touch</h3>
                 <p>
-                Book hotel rooms & get deals For the Best Price
+                  Phasellus nisi sapien, rutrum placerat sapien eu, rhoncus
+                  tempus felis. Nulla non pulvinar enim, vel viverra nunc
                 </p>
               </div>
               <div className="contact__area-info">
@@ -21,9 +22,7 @@ const Contactcontainer = () => {
                   <div className="contact__area-info-item-content">
                     <span>Emergency Help</span>
                     <h6>
-                      <a href="tel:+94(76) 170 6172">+94(76) 170 6172</a>
-                      <br/>
-                      <a href="tel:+94(76) 170 6172">+94(76) 733 6176</a>
+                      <a href="tel:+123(458)896895">+123 ( 458 ) 896 895</a>
                     </h6>
                   </div>
                 </div>
@@ -34,7 +33,7 @@ const Contactcontainer = () => {
                   <div className="contact__area-info-item-content">
                     <span>Quick Email</span>
                     <h6>
-                      <a href="mailto:rangiriviewresort@gmail.com">rangiriviewresort@gmail.com</a>
+                      <a href="mailto:support@gamil.com">support@gamil.com</a>
                     </h6>
                   </div>
                 </div>
@@ -43,9 +42,9 @@ const Contactcontainer = () => {
                     <i className="fal fa-map-marker-alt"></i>
                   </div>
                   <div className="contact__area-info-item-content">
-                    <span>Address</span>
+                    <span>Office Address</span>
                     <h6>
-                      <a href="#">Rathmalghaela Road, Temple Junction, Dambulla </a>
+                      <a href="#">GXF4+8HQ Chippenham United Kingdom </a>
                     </h6>
                   </div>
                 </div>
@@ -128,7 +127,7 @@ const Contactcontainer = () => {
             <div className="col-xl-12">
               <div className="contact__area-map section-padding">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830894606!2d-74.11976383964463!3d40.69766374865767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1652012644726!5m2!1sen!2sbd"
+                  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Rathmalghaela%20Road,%20Temple%20Junction,%20Dambulla+(IMA%20-%20Rangiri%20View%20Resort%20Dambulla)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                   loading="lazy"
                 ></iframe>
               </div>
